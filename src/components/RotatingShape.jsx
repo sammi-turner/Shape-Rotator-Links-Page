f827f5de1da37b16abbcaee01c0b1da750967585
Sammi Turner <ms.sammi.turner@gmail.com>
2024-10-20 16:42:34 +0100

    Add files via upload

---
+++ src/components/RotatingShape.jsx
@@ -0,0 +1,50 @@
+import React, { useRef, useState, useEffect } from 'react'
+import { useFrame } from '@react-three/fiber'
+import { Text } from '@react-three/drei'
+
+const RotatingShape = ({ ShapeComponent, args, linkText, url }) => {
+  const meshRef = useRef()
+  const [hovered, setHovered] = useState(false)
+
+  useFrame((state, delta) => {
+    if (meshRef.current) {
+      meshRef.current.rotation.x += delta * 0.2
+      meshRef.current.rotation.y += delta * 0.3
+    }
+  })
+
+  useEffect(() => {
+    document.body.style.cursor = hovered ? 'pointer' : 'default'
+  }, [hovered])
+
+  const handleClick = () => {
+    window.open(url, '_blank')
+  }
+
+  return (
+    <group>
+      <ShapeComponent 
+        ref={meshRef} 
+        args={args}
+        onClick={handleClick}
+        onPointerEnter={() => setHovered(true)}
+        onPointerLeave={() => setHovered(false)}
+      >
+        <meshBasicMaterial color={hovered ? "yellow" : "#39ff14"} wireframe />
+      </ShapeComponent>
+      <Text
+        position={[0, -2.1, 0]}
+        fontSize={0.4}
+        color={hovered ? "yellow" : "#39ff14"}
+        anchorX="center"
+        anchorY="middle"
+        maxWidth={2}
+        textAlign="center"
+      >
+        {linkText}
+      </Text>
+    </group>
+  )
+}
+
+export default RotatingShape
